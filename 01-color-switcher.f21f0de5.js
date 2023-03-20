f0e72c0fdba2309870b6e5dd5ce609e808c94601
AlenaRublik <114560252+AlenaRublik@users.noreply.github.com>
2023-03-20 20:34:28 +0000

    Deploying to gh-pages from @ AlenaRublik/goit-js-hw-09@c66a17cf715a9e12d29f727856033be295e0db05 🚀

---
+++ 01-color-switcher.f21f0de5.js
@@ -0,0 +1,2 @@
+!function(){startBtn=document.querySelector("button[data-start]"),stopBtn=document.querySelector("button[data-stop]");var t=null;startBtn.addEventListener("click",(function(){t=setInterval((function(){var t="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0));document.body.style.background=t}),1e3),startBtn.disabled=!0})),stopBtn.addEventListener("click",(function(){clearInterval(t),startBtn.disabled=!1}))}();
+//# sourceMappingURL=01-color-switcher.f21f0de5.js.map
