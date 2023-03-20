feb45b6f9185214602c512fd002343a816c26510
AlenaRublik <114560252+AlenaRublik@users.noreply.github.com>
2023-03-20 22:48:34 +0200

    fixing problems

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -2,8 +2,8 @@ function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
 }
 
-startBtn = document.querySelector('button[data-start]');
-stopBtn = document.querySelector('button[data-stop]');
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
 
 let startColor = null;
 
